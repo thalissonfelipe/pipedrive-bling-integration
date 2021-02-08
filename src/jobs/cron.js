cfb1a1f6503ec9721ed1876ab8342ecaf09143c1
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-02-08 09:26:58 -0300

    adding missiing import on cron.js file

--- src/jobs/cron.js
+++ src/jobs/cron.js
@@ -1,6 +1,7 @@
 const cron = require('node-cron');
 const logger = require('../utils/log');
 const { create } = require('../services/order.service');
+const historyRepository = require('../repositories/history.repository');
 
 /**
  * Cron used to run every day at 08:00 am.
@@ -11,7 +12,11 @@ cron.schedule('0 8 * * *', async () => {
     try {
         logger.info('Running Pipedrive and Bling integration.');
         await historyRepository.save();
-        await create();
+        const response = await create();
+        const message = response
+            ? 'Sync complete! New orders have been created.'
+            : 'Sync complete! No orders were created.';
+        logger.info(message);
     } catch (error) {
         logger.error(`Error on cron execution: ${error}`);
     }
